3ad3afb053c9ca28318f791875b7ce0cd417307b
micaelmelo1 <micaelmelo1@gmail.com>
2022-04-16 02:03:54 -0300

    update

--- script.js
+++ script.js
@@ -1,79 +1,93 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
 
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
+    // scroll-up button show/hide script
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
 
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $("html").css("scrollBehavior", "auto");
+  });
 
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
+  $(".navbar .menu li a").click(function () {
+    // applying again smooth scroll on menu items click
+    $("html").css("scrollBehavior", "smooth");
+  });
 
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["Júnior","Desenvolvedor Back-end", "Estudante"],
-        typeSpeed: 100,
-        cursorChar: "",
-        backSpeed: 60,
-        loop: true
-    });
+  // toggle menu/navbar script
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
 
-    var typed = new Typed(".typing-2", {
-        strings: ["Devenvolvedor Back-end", "Júnior"],
-        typeSpeed: 100,
-        cursorChar: "",
-        backSpeed: 60,
-        loop: true
-    });
+  // typing text animation script
+  var typed = new Typed(".typing", {
+    strings: ["Júnior", "Desenvolvedor Back-end", "Estudante"],
+    typeSpeed: 100,
+    cursorChar: "",
+    backSpeed: 60,
+    loop: true,
+  });
 
-    // owl carousel script
+  var typed = new Typed(".typing-2", {
+    strings: ["Devenvolvedor Back-end", "Júnior"],
+    typeSpeed: 100,
+    cursorChar: "",
+    backSpeed: 60,
+    loop: true,
+  });
 
+  // owl carousel script
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
 
-const skillsContent = document.getElementsByClassName('skills__content'),
-    skillsHeader = document.querySelectorAll('.skills__header')
+  const skillsContent = document.getElementsByClassName("skills__content"),
+    skillsHeader = document.querySelectorAll(".skills__header");
 
-function toggleSkills() {
-    let itemClass = this.parentNode.className
+  function toggleSkills() {
+    let itemClass = this.parentNode.className;
 
     for (i = 0; i < skillsContent.length; i++) {
-        skillsContent[i].className = 'skills__content skills__close'
+      skillsContent[i].className = "skills__content skills__close";
     }
-    if (itemClass === 'skills__content skills__close') {
-        this.parentNode.className = 'skills__content skills__open'
+    if (itemClass === "skills__content skills__close") {
+      this.parentNode.className = "skills__content skills__open";
     }
-}
+  }
 
-skillsHeader.forEach((el) => {
-    el.addEventListener('click', toggleSkills)
-})
-
-
+  skillsHeader.forEach((el) => {
+    el.addEventListener("click", toggleSkills);
+  });
 });
-
-
-
-
